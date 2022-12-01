b745a890c18c8310378a44e211c869cd81326886
Taylor00225 <478793660@qq.com>
2022-12-01 20:48:24 +0800

    0.0.1

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='decision_tree',
+    version='0.0.1',
+    packages=['decision_tree'],
+    url='https://github.com/Taylor00225/decision_tree',
+    license='',
+    author='Taylor00225',
+    author_email='478793660@qq.com',
+    description='A simple demo of decision_tree including ID3 and C4.5.',
+    python_requires='>=3.8',
+    install_requires=[
+        "numpy>=1.5.2",
+        "numpy>=1.23.5",
+        "openpyxl>=3.0.10"
+    ]
+)
