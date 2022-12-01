a26c8a0d0e6d233e90c5221601da8b45c50a6cc2
Taylor00225 <478793660@qq.com>
2022-12-01 20:55:54 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='A simple demo of decision_tree including ID3 and C4.5.',
     python_requires='>=3.8',
     install_requires=[
-        "numpy>=1.5.2",
+        "pandas>=1.5.2",
         "numpy>=1.23.5",
         "openpyxl>=3.0.10"
     ]
