02fa53a05ac0d593372c58af58d5e78e2fa9191c
Taylor00225 <111715975+Taylor00225@users.noreply.github.com>
2022-12-10 11:24:50 +0800

    0.1.1

--- source/main.py
+++ source/main.py
@@ -14,12 +14,14 @@ if __name__ == "__main__":
     c45root = Node(None, -1)
     try:
         base.create_tree(id3root, dataframe, labels, base.get_gain)
+        print("ID3")
         base.print_tree(id3root, [])
     except Exception as err:
         print("ID3 " + str(err))
         sys.exit(1)
     try:
         base.create_tree(c45root, dataframe, labels, base.get_gain_ratio)
+        print("C4.5")
         base.print_tree(c45root, [])
     except Exception as err:
         print("C4.5 " + str(err))
@@ -31,20 +33,24 @@ if __name__ == "__main__":
     data_c45_pred = []
     data_true = []
     while i < n:
+        print("*******************************")
         data = test_dataframe.iloc[i]
         data_true.append(data['类别'])
         print(data, end='\n\n')
 
-        print("ID3:")
+        print("ID3:", end='')
         pred = base.decision(id3root, data)
         data_id3_pred.append(pred)
         print(pred)
 
-        print("C45:")
+        print("C45:", end='')
         pred = base.decision(c45root, data)
         data_c45_pred.append(pred)
-        print(pred, end='\n\n')
+        print(pred, end='\n')
         i += 1
 
-    print(base.get_accuracy_score(data_id3_pred, data_true), sep='ID3 accuracy: ')
-    print(base.get_accuracy_score(data_c45_pred, data_true), sep='C4.5 accuracy: ')
+    print("*******************************")
+    print("ID3 accuracy: ", end='')
+    print(base.get_accuracy_score(data_id3_pred, data_true))
+    print("C4.5 accuracy: ", end='')
+    print(base.get_accuracy_score(data_c45_pred, data_true))
